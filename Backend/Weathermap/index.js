56d44dd86198e113ffc0da1ac3ee7615af91f898
Heehyun-Ryu <rhh221@naver.com>
2023-11-05 16:50:27 +0900

    express 서버 구현

---
+++ Backend/Weathermap/index.js
@@ -0,0 +1,13 @@
+const express = require('express')      //라이브러리 첨부
+const app = express();                  //객체 만드는 건데 몰라도 됨
+
+app.listen(8080,function(){
+	console.log('listening on 8080')
+});
+
+//누군가가 /pet 으로 방문을 하면..
+//pet관련된 안내문을 띄워주자
+
+app.get('/',function(요청,응답){
+	응답.send('반갑습니다');
+});
